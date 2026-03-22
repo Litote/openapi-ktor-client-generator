0927f88a933341228b2be58f7bb700e0920eaed2
Julien Buret <jburet@gmail.com>
2026-03-22 07:04:07 +0100

    chore: fix e2e-split job - comment the generated include

--- e2e-split/settings.gradle.kts
+++ e2e-split/settings.gradle.kts
@@ -17,5 +17,4 @@ rootProject.name = "e2e-split"
 
 
 // <openapi-ktor-generated-includes>
-include(":client:shared", ":client:shared-userusersget-useruserspost", ":client:shared-orderordersget-orderorderspost", ":client:user-users-get-client", ":client:user-users-post-client", ":client:order-orders-get-client", ":client:order-orders-post-client")
 // </openapi-ktor-generated-includes>
